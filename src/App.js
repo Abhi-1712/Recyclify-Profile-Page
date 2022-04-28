60068e49e604440e4987805bd08bf932725100fb
Abhi-1712 <abhijeet171202@gmail.com>
2022-04-28 22:39:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from "./Profile/nav";
+import Header from "./Profile/Header";
+import PageContent from "./Profile/pageContent";
+import Footer from "./Profile/Footer"
+function App() {
+  return (
+    <div>
+    <Home/>
+    <Header/>
+    <PageContent/>
+    <Footer/>
+    </div>
+  );
+  
+}
+
+export default App;
